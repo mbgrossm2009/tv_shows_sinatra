cecd4b304f2dec2ecce819915ece93ff1a684555
Matthew Grossman <grossmanmatt@yahoo.com>
2017-02-19 23:50:59 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "https://rubygems.org"
+
+gem "activerecord", "~> 4.2"
+gem "sinatra"
+gem "sinatra-activerecord"
+gem "pg"
+gem "rake"
+gem "pry"
+
+group :development, :test do
+  gem "rspec"
+  gem "capybara"
+  gem "database_cleaner"
+  gem "pry"
+end
---
+++ server.rb
@@ -0,0 +1,38 @@
+require "sinatra"
+require "sinatra/activerecord"
+require_relative "app/models/television_show"
+require "pry"
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+set :views, File.join(File.dirname(__FILE__), "app/views")
+
+get "/" do
+  redirect "/television_shows"
+end
+
+get "/television_shows" do
+  @shows = TelevisionShow.all
+  erb :index
+end
+
+get "/television_shows/new" do
+  @show = TelevisionShow.new
+  erb :new
+end
+
+get "/television_shows/:id" do
+  @show = TelevisionShow.find(params[:id])
+  erb :show
+end
+
+post "/television_shows" do
+  @show = TelevisionShow.new(params[:television_show])
+  if @show.save
+    # binding.pry
+    redirect "/television_shows"
+  else
+    @errors = @show.errors.full_messages
+    # binding.pry
+    erb :new
+  end
+end
